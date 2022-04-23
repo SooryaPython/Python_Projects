0e7bb814079a0fb4748bc9cf0f35ff0dd626ab8b
SooryaPython <104213218+SooryaPython@users.noreply.github.com>
2022-04-23 19:16:31 +0530

    Add files via upload

---
+++ About GitHub.py
@@ -0,0 +1,16 @@
+"""
+Date: 23rd April 2022
+Name: S. Soorya
+File description: For practice
+"""
+# This file is about GitHub
+a_var="About GitHub:" # a variable
+print(a_var)
+print("""GitHub, Inc. is a provider of Internet hosting for software development and version control using Git. It offers the distributed version control and source code management 
+(SCM) functionality
+ of Git, plus its own features. It provides access control and several collaboration features such as bug tracking, feature
+  requests, task management, continuous integration and wikis for every project. 
+  Headquartered in California, it has been a subsidiary of Microsoft since 2018.""")
+print("""Source: Wikipedia""")
+
+# Program finished.
