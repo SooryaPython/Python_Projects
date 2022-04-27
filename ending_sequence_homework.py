8b7540ec8d4bdffb7e7f00cd971bf4db991752f1
SooryaPython <104213218+SooryaPython@users.noreply.github.com>
2022-04-27 20:08:43 +0530

    Add files via upload

---
+++ ending_sequence_homework.py
@@ -0,0 +1,34 @@
+"""
+Date: 27th April 2022
+Name: S. Soorya
+File description: About escape sequence in Python
+"""
+# H.W: To print names from a to z starting with a backslash. Example: hello\akash
+print("hello\adhyanta prabhu") # hellodhyanta prabhu
+print("hello\brijesh") # hellorijesh
+print("hello\catherine") # hello\catherine
+print("hello\dakshesh") # hello\dakshesh
+print("hello\elangovan") # hello\elangovan
+print("hello\finn") # hello
+                    #  inn 
+print("hello\ganesh") # hello\ganesh
+print("hello\harshan") #hello\harshan
+print("hello\iyappan") # hello\iyappan
+print("hello\jerome") # hello\jerome
+print("hello\kannan") # hello\kannan
+print("hello\lawrence") # hello\lawrence
+print("hello\manikandan") # hello\manikandan
+print("hello\newton") # hello
+                      # newton
+print("hello\octane") # hello\octane
+print("hello\pradeep") # hello\pradeep
+print("hello\quinn") # hello\quinn 
+print("hello\ram") # amllo
+print("hello\soorya") # hello\soorya
+print("hello\tanya") # hello   anya
+# print("hello\uthish") #SyntaxError
+print("hello\vanhelsing") #        anhelsing
+print("hello\walter") #hello\walter
+# print("hello\xavier") #SyntaxError
+print("hello\yash") # hello\yash
+print("hello\zoro") # hello\zoro
