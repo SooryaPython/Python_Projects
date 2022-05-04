0b6cc11155e773ac32d2a29d70f839ab39c35cb4
SooryaPython <104213218+SooryaPython@users.noreply.github.com>
2022-05-04 15:51:32 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,23 @@
+""" 
+Date: 4rd May 2022
+Name: S. Soorya
+File description: About input in Python
+"""
+# This is a task
+
+a=input("Print your Name:")
+b=input("School \ College Name:")
+c=int(input("Your Roll Number:"))
+d=float(input("Your Percemtage:"))
+e=float(input("Your Height"))
+f=input("Your Favourite Movie:")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+print("My Name is",a,"School\College Name is",b,"My Roll Number is",c,"My Percentage is",d,"My Height is",e,"My Favourite Movie is",f)
+
+# Program Finished.
